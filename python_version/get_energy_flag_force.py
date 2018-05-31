ac5c766a22e4f0929d0a1bb57c48f0210b73aca9
wangye_ndt <y.wang@newdegreetech.com>
2018-05-31 20:06:32 +0800

    增加基线跟踪算法
    在energy, flag优化的基础上, 增加了对原始信号进行基线跟踪算法, 并将功能整合到DataProcessing类

--- python_version/get_energy_flag_force.py
+++ python_version/get_energy_flag_force.py
@@ -7,118 +7,142 @@ import pandas as pd
 import sys
 
 
-class CalcEnergy:
-    def __init__(self, alpha, beta):
-        self.alpha = alpha
-        self.beta = beta
+class DataProcessing:
+    def __init__(self, data):
+        self.data = data
+        self.base = data
+        self.force_signal = data
+        self.energy = None
+        self.flag = None
+        self.force = None
+        self.mov_avg_len = 5
+        self.sigma_wave = 10
+        self.sigma_tsunami = 4
+        self.alpha = 3
+        self.beta = 5
+        self.energy_thd = 30
+        self.upper = self.energy_thd
+        self.lower = self.energy_thd
+        self.step_u = 0
+        self.step_l = 0
+        self.bef = 50
+        self.aft = 50
+        self.avg = 10
 
-    def calc_energy(self, data):
-        data = np.array(data)
-        if data.ndim == 1:
-            data = data[:, np.newaxis]
-        m = data.shape[0]
+    def pre_process(self):
+        if np.ndim(self.data) == 1:
+            self.data = self.data - self.data[0]
+            self.data = self.data[:, np.newaxis]
+        else:
+            self.data = self.data - self.data[0, :]
+        self.data = -self.data
+
+    def calc_moving_avg(self):
+        output = []
+        for ii in range(len(self.data)):
+            if ii <= self.mov_avg_len:
+                output.append(np.mean(self.data[:ii + 1, :], 0))
+            else:
+                output.append(np.mean(self.data[ii - (self.mov_avg_len - 1):ii + 1, :], 0))
+        self.data = np.array(output)
+
+    def baseline_removal_single(self, data):
+        base = np.copy(data)
+        for ii in range(1, len(data)):
+            base[ii] = base[ii - 1] + \
+                       (data[ii] - data[ii - 1]) * \
+                       np.exp(-(data[ii] - data[ii - 1]) ** 2 / self.sigma_wave) + \
+                       (data[ii - 1] - base[ii - 1]) * \
+                       np.exp(-np.abs(data[ii - 1] - base[ii - 1]) / self.sigma_tsunami)
+        return base
+
+    def baseline_removal(self):
+        self.base = np.apply_along_axis(
+            lambda x: self.baseline_removal_single(x), 0, self.data
+        )
+        self.force_signal = self.data - self.base
+
+    def calc_energy(self):
+        self.force_signal = np.array(self.force_signal)
+        if self.force_signal.ndim == 1:
+            self.force_signal = self.force_signal[:, np.newaxis]
+        m = self.force_signal.shape[0]
         energy_n = np.zeros((m, 1))
         for ii in range(m - self.alpha - self.beta):
-            energy_n[ii + self.alpha + self.beta] = 1 / self.alpha * np.sum(
-                np.sum(np.abs(data[ii + self.beta:ii + self.beta + self.alpha, :] - data[ii:ii + self.alpha, :]), 1), 0)
-            # diff_mat = data[ii + self.beta:ii + self.beta + self.alpha, :] - data[ii:ii + self.alpha, :]
+            energy_n[ii + self.alpha + self.beta] = \
+                1 / self.alpha * \
+                np.sum(np.sum(np.abs(
+                    self.force_signal[ii + self.beta:ii + self.beta + self.alpha, :] -
+                    self.force_signal[ii:ii + self.alpha, :]), 1), 0)
+            # diff_mat = self.force_signal[ii + self.beta:ii + self.beta + self.alpha, :] - self.force_signal[ii:ii + self.alpha, :]
             # diff_mat_max_sub = np.array(np.where(np.abs(diff_mat) == np.max(np.abs(diff_mat))))
             # diff_mat_max_sub = diff_mat_max_sub[:, 0]
             # max_sign = np.sign(diff_mat[diff_mat_max_sub[0], diff_mat_max_sub[1]])
             # energy_n[ii + self.alpha + self.beta] *= max_sign
-        return energy_n.T[0]
+        self.energy = energy_n.T[0]
 
+    # t is tvalue < thd
+    @staticmethod
+    def update_flag_status(f, r, t):
+        f = f ^ r and f or not (f ^ r) and not (f ^ t)
+        r = not r and f and t or r and not (not f and t)
+        return f, r
 
-def preprocess(data):
-    if np.ndim(data) == 1:
-        data = data - data[0]
-    else:
-        data = data - data[0, :]
-    return data
+    def calc_flag(self):
+        self.flag = np.zeros(self.energy.shape, dtype=np.bool)
+        ready = False
+        for ii in range(1, self.flag.shape[0]):
+            f = bool(self.flag[ii - 1])
+            t = (not f and (self.energy[ii] < self.energy_thd)) or (f and (self.energy[ii] < self.energy_thd * 0.9))
+            self.flag[ii], ready = self.update_flag_status(f, ready, t)
+        self.flag = np.array(self.flag, dtype=np.int)
 
+    def show_fig(self, f):
+        fig = plt.figure()
+        fig.set_size_inches(60, 10)
+        plt.plot(self.force_signal, '-', linewidth=3)
+        plt.plot(self.energy)
+        plt.hlines(self.upper, 0, self.data.shape[0], linestyles='--')
+        plt.hlines(self.lower, 0, self.data.shape[0], linestyles='--')
+        plt.plot(self.flag * (np.max(self.data) - np.min(self.data)) + np.min(self.data), '--')
+        plt.title(f)
+        plt.legend(['rawdata1', 'rawdata2', 'energy', 'upper limit', 'lower limit', 'touch flag'])
+        plt.show()
 
-def calc_moving_avg(data, winlen):
-    output = []
-    for ii in range(len(data)):
-        if ii <= winlen:
-            output.append(np.mean(data[:ii + 1, :], 0))
-        else:
-            output.append(np.mean(data[ii - (winlen - 1):ii + 1, :], 0))
-    return np.array(output)
-
-
-def calc_energy(data, alpha, beta):
-    ce = CalcEnergy(alpha, beta)
-    energy = ce.calc_energy(data)
-    return energy
-
-
-# t is tvalue < thd
-def update_flag_status(f, r, t):
-    f = f ^ r and f or not (f ^ r) and not (f ^ t)
-    r = not r and f and t or r and not (not f and t)
-    return f, r
-
-
-def calc_flag(energy, thd):
-    flag = np.zeros(energy.shape, dtype=np.bool)
-    ready = False
-    for ii in range(1, flag.shape[0]):
-        f = bool(flag[ii - 1])
-        t = (not f and (energy[ii] < thd)) or (f and (energy[ii] < thd * 0.9))
-        flag[ii], ready = update_flag_status(f, ready, t)
-    return flag
-
-
-def show_fig(data, energy, flag, params,f):
-    fig = plt.figure()
-    fig.set_size_inches(60, 10)
-    plt.plot(data, '-', linewidth=3)
-    plt.plot(energy)
-    plt.hlines(params[0], 0, data.shape[0], linestyles='--')
-    plt.hlines(params[1], 0, data.shape[0], linestyles='--')
-    plt.plot(flag * (np.max(data) - np.min(data)) + np.min(data), '--')
-    plt.title(f)
-    plt.legend(['rawdata1', 'rawdata2', 'energy', 'upper limit', 'lower limit', 'touch flag'])
-    plt.show()
-
-
-def calc_force(data_, flag_, bfm_len, aft_len, avg_len, force):
-    tds = np.array(np.where(np.diff(flag_) == 1))[0]
-    if np.ndim(data_) == 1:
-        data_ = data_[:, np.newaxis]
-    params = np.zeros((data_.shape[1], tds.shape[0]))
-    for ii in range(params.shape[1]):
-        params[:, ii] = np.mean(data_[tds[ii] + aft_len:tds[ii] + aft_len + avg_len, :], 0) - np.mean(
-            data_[tds[ii] - bfm_len - avg_len:tds[ii] - bfm_len, :], 0)
-        # print('base', np.mean(data_[tds[ii] + aft_len:tds[ii] + aft_len + avg_len, :], 0))
-        # print('peak', np.mean(data_[tds[ii] - bfm_len - avg_len:tds[ii] - bfm_len, :], 0))
-    return params.T
+    def calc_force(self):
+        tds = np.array(np.where(np.diff(self.flag) == 1))[0]
+        if np.ndim(self.force_signal) == 1:
+            self.force_signal = self.force_signal[:, np.newaxis]
+        params = np.zeros((self.force_signal.shape[1], tds.shape[0]))
+        for ii in range(params.shape[1]):
+            params[:, ii] = np.mean(self.force_signal[tds[ii] + self.aft:tds[ii] + self.aft + self.avg, :], 0) - \
+                            np.mean(self.force_signal[tds[ii] - self.bef - self.avg:tds[ii] - self.bef, :], 0)
+        self.force = params.T
 
 
 if __name__ == '__main__':
-    path = 'RawData'
-    files = os.listdir(path)
-    for f in files:
-        if '.txt' in f:
-            data = pd.read_table(os.path.join(path, f), sep=',', header=None)
-            data = np.array(data.ix[:, 16:21])
-            alpha = 3
-            beta = 5
-            energy_thd = 100
-            upper = lower = energy_thd
-            step_u = 0
-            step_l = 0
-            bef = 50
-            aft = 50
-            avg = 10
-            data = preprocess(data)
-            data = calc_moving_avg(data, 3)
-            # plt.plot(data)
-            energy = calc_energy(data, alpha, beta)
-            flag = np.array(calc_flag(energy, energy_thd), dtype=np.int)
-            down_array = np.where(np.diff(flag) == 1)
-            up_array = np.where(np.diff(flag) == -1)
-            show_fig(data, energy, flag, [upper, lower], f)
-            force = calc_force(data, flag, bef, aft, avg, 100)
-            np.savetxt(''.join((f[:-4], '_test.txt')), force, fmt='%8.2f%8.2f%8.2f%8.2f%8.2f%8.2f')
+    path = '.'
+    dirs = os.listdir(path)
+    for d in dirs:
+        if not os.path.isdir(d):
+            continue
+        files = os.listdir(os.path.join(path, d))
+        for f in files:
+            if '.txt' in f:
+                data = np.genfromtxt(os.path.join(path, d, f), delimiter=',')
+                data = data[:, [0, 4]]
+                data = data[:, 0]
+                dp = DataProcessing(data)
+                dp.pre_process()
+                dp.calc_moving_avg()
+                dp.baseline_removal()
+                plt.plot(dp.data)
+                plt.plot(dp.base)
+                plt.plot(dp.data - dp.base)
+                plt.show()
+                dp.calc_energy()
+                dp.calc_flag()
+                dp.show_fig(f)
+                dp.calc_force()
+                force = dp.force
+                np.savetxt(''.join((f[:-4], '_test.txt')), force, fmt='%8.2f')
