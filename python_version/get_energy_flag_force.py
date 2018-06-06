834c8424701a6a0a4836ddfc07626c7815a57c69
wangye_ndt <y.wang@newdegreetech.com>
2018-06-06 16:50:20 +0800

    计算flag的时候加入了时间保护
    计算flag的模块中加入了时间保护, touch down一定时间后才可touch up, touch up一段时间后才可再次touch down

--- python_version/get_energy_flag_force.py
+++ python_version/get_energy_flag_force.py
@@ -21,6 +21,8 @@ class DataProcessing:
         self.alpha = 3
         self.beta = 5
         self.energy_thd = 30
+        self.min_td_time = 50
+        self.min_tu_time = 50
         self.upper = self.energy_thd
         self.lower = self.energy_thd
         self.step_u = 0
@@ -83,18 +85,24 @@ class DataProcessing:
 
     # t is tvalue < thd
     @staticmethod
-    def update_flag_status(f, r, t):
-        f = f ^ r and f or not (f ^ r) and not (f ^ t)
-        r = not r and f and t or r and not (not f and t)
-        return f, r
+    def update_flag_status(f, r, t, tdf, tuf):
+        f_ = f ^ r and f or not (f ^ r) and not (f ^ t)
+        f_ = f_ and tuf if not f else f_ or not tdf
+        r_ = not r and f and t or r and not (not f and t)
+        return f_, r_
 
     def calc_flag(self):
         self.flag = np.zeros(self.energy.shape, dtype=np.bool)
         ready = False
+        touch_down_frm = touch_up_frm = 0
         for ii in range(1, self.flag.shape[0]):
             f = bool(self.flag[ii - 1])
             t = (not f and (self.energy[ii] < self.energy_thd)) or (f and (self.energy[ii] < self.energy_thd * 0.9))
-            self.flag[ii], ready = self.update_flag_status(f, ready, t)
+            touch_down_frm = touch_down_frm + 1 if self.flag[ii - 1] else 0
+            touch_up_frm = touch_up_frm + 1 if not self.flag[ii - 1] else 0
+            tdf = touch_down_frm >= self.min_td_time
+            tuf = touch_up_frm >= self.min_tu_time
+            self.flag[ii], ready = self.update_flag_status(f, ready, t, tdf, tuf)
         self.flag = np.array(self.flag, dtype=np.int)
 
     def show_fig(self, f):
